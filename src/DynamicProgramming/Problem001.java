5401adc4cdb9fcc92ce90b4876ea9d9781f0fed9
ygh <3249724402@qq.com>
2024-07-08 19:48:52 +0800

    test

--- src/DynamicProgramming/Problem001.java
+++ src/DynamicProgramming/Problem001.java
@@ -1,6 +1,29 @@
 package DynamicProgramming;
 
-public class Problem001 {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Problem001 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int number = rand.nextInt(100)+1;
+        Scanner sc = new Scanner(System.in);
+//        int number =35;
+
+        while(true){
+            System.out.println("Please enter a number between 1 and 100");
+            int guess=sc.nextInt();
+//            int guess=56;
+            if(guess==number){
+                System.out.println("Congratulations! You win!");
+                break;
+            }else if(guess<number){
+                System.out.println("Sorry, your guess is too low!");
+            }else {
+                System.out.println("Sorry, your guess is too high!");
+            }
+        }
+
+    }
 
 }
