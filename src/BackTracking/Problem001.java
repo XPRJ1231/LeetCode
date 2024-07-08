e45642210c8187a56d9e72aa99c1467fdb45d319
ygh <3249724402@qq.com>
2024-07-08 21:29:10 +0800

    初始文件

--- src/BackTracking/Problem001.java
+++ src/BackTracking/Problem001.java
@@ -2,24 +2,6 @@ package BackTracking;
 
 public class Problem001 {
     public static void main(String[] args) {
-        Random rand = new Random();
-        int number = rand.nextInt(100)+1;
-        Scanner sc = new Scanner(System.in);
-//        int number =35;
-
-        while(true){
-            System.out.println("Please enter a number between 1 and 100");
-            int guess=sc.nextInt();
-//            int guess=56;
-            if(guess==number){
-                System.out.println("Congratulations! You win!");
-                break;
-            }else if(guess<number){
-                System.out.println("Sorry, your guess is too low!");
-            }else {
-                System.out.println("Sorry, your guess is too high!");
-            }
-        }
 
     }
 }
